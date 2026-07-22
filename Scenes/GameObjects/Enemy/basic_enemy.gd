1ee1bb9f27c22a74e7bb65d9b62268616a78179d
Yrainy0419 <lily.tan12345678910@gmail.com>
2026-07-22 19:19:42 +0800

    Animating Enemy Death

--- Scenes/GameObjects/Enemy/basic_enemy.gd
+++ Scenes/GameObjects/Enemy/basic_enemy.gd
@@ -3,6 +3,7 @@ extends CharacterBody2D
 const MAX_SPEED = 50
 
 @onready var health_component : HealthComponent = $HealthComponent
+@onready var visuals = $Visuals
 
 
 func _process(_delta):
@@ -10,6 +11,11 @@ func _process(_delta):
 	
 	velocity = direction * MAX_SPEED
 	move_and_slide()
+	
+	var move_sign = sign(velocity.x)
+	
+	if move_sign != 0:
+		visuals.scale = Vector2(-move_sign, 1)
 
 
 func get_direction_to_player():
