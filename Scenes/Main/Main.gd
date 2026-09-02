dd29ac3f4bfbe0142834bc795bb83a9473d7a054
Yrainy0419 <lily.tan12345678910@gmail.com>
2026-09-02 16:39:00 +0800

    Creating a Pause Menu

--- Scenes/Main/Main.gd
+++ Scenes/Main/Main.gd
@@ -2,10 +2,18 @@ extends Node
 
 @export var end_screen_scene : PackedScene
 
+var pause_menu_scene = preload("res://Scenes/UI/pause_menu.tscn")
+
 
 # 监听玩家死亡信号
 func _ready():
 	$%Player.health_component.died.connect(on_player_died)
+
+
+func _unhandled_input(event):
+	if event.is_action_pressed("pause"):
+		add_child(pause_menu_scene.instantiate())
+		get_tree().root.set_input_as_handled()
 
 
 # 玩家死亡后显示失败界面
@@ -15,4 +23,3 @@ func on_player_died():
 	add_child(end_screen_instance)
 	end_screen_instance.set_defeat()
 	
-
