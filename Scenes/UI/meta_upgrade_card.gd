f38e443c504a0bda6ee3857b54da86bdaf645415
Yrainy0419 <lily.tan12345678910@gmail.com>
2026-09-09 14:53:35 +0800

    Creating the Meta Upgrade Card

---
+++ Scenes/UI/meta_upgrade_card.gd
@@ -0,0 +1,25 @@
+extends PanelContainer
+
+@onready var name_label: Label = $%NameLabel
+@onready var description_label : Label = $%DescriptionLabel
+
+
+# 监听卡片的输入事件
+func _ready():
+	gui_input.connect(on_gui_input)
+
+
+# 显示升级的名称和说明
+func set_meta_upgrade(upgrade : MetaUpgrade):
+	name_label.text = upgrade.name
+	description_label.text = upgrade.description
+
+
+func select_card():
+	$AnimationPlayer.play("selected")
+
+
+# 点击卡片时发出选择信号
+func on_gui_input(event : InputEvent):
+	if event.is_action_pressed("left_click"):
+		select_card()
