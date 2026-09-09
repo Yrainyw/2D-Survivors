479795cf3bd7d7e4e8b56cab2491e6150f4df450
Yrainy0419 <lily.tan12345678910@gmail.com>
2026-09-09 17:38:16 +0800

    Adding a ScrollContainer to the Meta Upgrade Sceen

--- Scenes/UI/meta_menu.gd
+++ Scenes/UI/meta_menu.gd
@@ -13,10 +13,10 @@ func _ready():
 	
 	for child in grid_container.get_children():
 		child.queue_free()
-	
+
 	for upgrade in upgrades:
 		var meta_upgrade_card_instance = meta_upgrade_card_scene.instantiate()
-		
+
 		grid_container.add_child(meta_upgrade_card_instance)
 		meta_upgrade_card_instance.set_meta_upgrade(upgrade)
 
